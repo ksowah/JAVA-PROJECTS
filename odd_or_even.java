518827b1a80e973ae36b4528df30020f270b8111
kelvin_xs <80095257+ksowah@users.noreply.github.com>
2021-08-11 01:05:06 +0000

    Add files via upload

---
+++ odd_or_even.java
@@ -0,0 +1,25 @@
+package com.Practice;
+import java.util.Scanner;
+
+ class TakeInput {
+
+
+
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter number: ");
+        Scanner numInput = new Scanner(System.in);
+        num = numInput.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("\'" + num +"\'" + " is an even number");
+        }else{
+            System.out.println("\'" + num +"\'" + " is an odd number");
+        }
+    }
+
+}
+
+
+
+
