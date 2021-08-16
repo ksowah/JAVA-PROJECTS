7e02e03e32be4a11cf2c67c02960c4746d5dceee
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-08-16 20:29:41 +0000

    Update names_sorter.java

--- names_sorter.java
+++ names_sorter.java
@@ -9,7 +9,7 @@ public class Main{
         public static void main(String[] args) {
 
             int numOfList;
-
+        // user enters number of items
             System.out.println("Enter number of names: ");
 
             Scanner number = new Scanner(System.in);
