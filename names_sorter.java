86986efaf525e94c79f04c0f05b5dae56b8e58e2
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-08-16 20:28:26 +0000

    Add files via upload
    This is a simple java program that sorts a list of names in alphabetical order

---
+++ names_sorter.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main{
+
+
+
+        public static void main(String[] args) {
+
+            int numOfList;
+
+            System.out.println("Enter number of names: ");
+
+            Scanner number = new Scanner(System.in);
+            numOfList = number.nextInt();
+
+            System.out.println("Please enter list of names");
+
+            ArrayList<String> names = new ArrayList<>();
+            Scanner name = new Scanner(System.in);
+
+
+
+            for (int i = 1; i <= numOfList; i++){
+
+                names.add(name.nextLine());
+            }
+
+            Collections.sort(names);
+
+            System.out.println();
+            System.out.println("The sorted list is below: ");
+            System.out.println();
+
+
+            for (String s : names) {
+                System.out.println(s);
+            }
+            
+
+            }
+
+    }
+
+
+
+
+
