af8fa24215bf1a830352e2293ce6199cd5143f90
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-08-19 22:04:52 +0000

    Update decimal_to_octal.java

--- decimal_to_octal.java
+++ decimal_to_octal.java
@@ -10,12 +10,15 @@ import java.util.Scanner;
     int[] octal = new int[80];
     int i = 0;
 
+        // take user input
         System.out.println("Enter decimal number: ");
         Scanner input = new Scanner(System.in);
         decimal = input.nextInt();
 
+        // user input is copied to another variable
         int copyOfDec = decimal;
-
+     
+         // loops through the given decimal number
         while (decimal != 0){
 
             octal[i] = decimal % 8;
@@ -26,6 +29,8 @@ import java.util.Scanner;
 
         System.out.println("'" + copyOfDec + "'" + " in octal is: ");
 
+     
+        // the looped results is then looped again but in reverse order
         for (int j = i - 1; j >= 0; j--){
 
             System.out.print(octal[j]);
