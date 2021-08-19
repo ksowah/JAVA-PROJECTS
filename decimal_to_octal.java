adaa91bafac1770baa50171b83ab96cbfa462597
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-08-19 21:56:59 +0000

    Add files via upload
    simple java program that converts decimal to octal

---
+++ decimal_to_octal.java
@@ -0,0 +1,44 @@
+package com.Practice;
+import java.util.Scanner;
+ class Main {
+
+
+
+
+    public static void main(String[] args) {
+
+    int decimal;
+    int[] octal = new int[80];
+    int i = 0;
+
+        System.out.println("Enter decimal number: ");
+        Scanner input = new Scanner(System.in);
+        decimal = input.nextInt();
+
+        int copyOfDec = decimal;
+
+        while (decimal != 0){
+
+            octal[i] = decimal % 8;
+            decimal /= 8;
+            i++;
+
+        }
+
+        System.out.println("'" + copyOfDec + "'" + " in octal is: ");
+
+        for (int j = i - 1; j >= 0; j--){
+
+            System.out.print(octal[j]);
+
+        }
+
+        System.out.println();
+
+    }
+
+}
+
+
+
+
